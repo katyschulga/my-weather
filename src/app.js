4402f017591fba80e15a0ae102fbf4fe751b8161
Katy Schulga <86527729+katyschulga@users.noreply.github.com>
2024-09-10 14:27:38 +0100

    added updates js for data update through api info

--- src/app.js
+++ src/app.js
@@ -7,8 +7,10 @@ function refreshWeather(response) {
   let windspeedElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   console.log(response.data);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
